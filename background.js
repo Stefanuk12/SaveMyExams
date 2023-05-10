5a4d90b08d337f56ffad7343d889b210dd87dcd0
Stefan <stefanukpadd@gmail.com>
2023-05-10 13:41:13 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,34 @@
+// Vars
+const BaseURL = "http://localhost:8080"
+const SiteRE = /https:\/\/www\.savemyexams\.co\.uk\/api\/v0\/posts\/([\w\_-~]+)\/topic-questions\/parts\/([\w-~\_]+)\/solution/
+const Proxy = {
+    solution: `${BaseURL}/solution`
+}
+
+// See whenever we visit a site
+chrome.webRequest.onBeforeRequest.addListener(
+    // Modifier
+    function(details) {
+        // Make sure is a request for a solution
+        const URLMatch = SiteRE.exec(details.url)
+        if (!URLMatch) {
+            return
+        }
+
+        // Return our modified request (redirect)
+        const modifiedRequest = {
+            redirectUrl: Proxy.solution
+        }
+        return modifiedRequest
+    },
+
+    // Filter
+    {
+        urls: [
+            "https://www.savemyexams.co.uk/*"
+        ]
+    },
+    [
+        "blocking"
+    ]
+)
