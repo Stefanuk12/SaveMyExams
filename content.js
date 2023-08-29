21004f96d38aaa1bc7d224474683c8252b191058
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2023-08-29 22:51:36 +0100

    Update + Made `content.js` async and to wait for Sme to fully load in + Added tracking uri to `rules.json`

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
-// Reset our note reviews
-window.Sme.helpers.ls.removeItem("SME.revision-note-views")
+(async() => {
+    // Allow things to load in
+    while (window.Sme.helpers === undefined) {
+        await new Promise(resolve => setTimeout(resolve, 1))
+    }
 
-// Disable tracking
-window.Sme.track.sendGAEvent = function() { return }
-window.Sme.track.event = function() { return }
+    // Reset our note reviews
+    window.Sme.helpers.ls.removeItem("SME.revision-note-views")
 
-// Log loaded in console
-console.log("%c" + "Loaded SME crack!", "color: #ff9696; font-size: 50px; font-weight: bold;");
+    // Disable tracking
+    window.Sme.track.sendGAEvent = function() { return }
+    window.Sme.track.event = function() { return }
+    window.Sme.track.eventOnReady = function() { return }
+
+    // Log loaded in console
+    console.log("%c" + "Loaded SME crack!", "color: #ff9696; font-size: 50px; font-weight: bold;");
+})();
